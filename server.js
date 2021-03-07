3d867d1c71902ebd61373ce575cb7b2d16813d6a
William Cheausky <williamchenausky@yahoo.com>
2021-03-07 11:11:18 -0500

    make it better

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const fs = require('fs')
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
