f9723dd0f6ae5f6840ad15ff8f93d36c81e1aaa4
William Cheausky <williamchenausky@yahoo.com>
2021-03-08 22:12:49 -0500

    update routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,4 +8,8 @@ module.exports = (app) => {
     app.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     })
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html' ));
+    });
 }
