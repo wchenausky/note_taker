c8373efb9640f86ad56851384f281ba9b6172b89
William Cheausky <williamchenausky@yahoo.com>
2021-03-11 09:56:44 -0500

    update routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,15 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = (app) => {
-    app.get('/', (req,res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html' ));
-    });
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
 
-    app.get('/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/notes.html'));
-    })
+  app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
 
-    app.get('*', (req,res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html' ));
-    });
-}
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
